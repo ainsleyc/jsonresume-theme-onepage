c8938d6d91056a7a80e73560af7dcc8627d7b54f
Ainsley Chong <ainsley@factual.com>
2015-03-11 07:16:11 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var Handlebars = require("handlebars");
+
+function render(resume) {
+	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resume
+	});
+}
+
+module.exports = {
+	render: render
+};
