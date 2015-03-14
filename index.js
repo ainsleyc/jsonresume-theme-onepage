5ee48fc82122c24fe564e8955e7ca4f5eb46cc7c
Ainsley Chong <ainsley@factual.com>
2015-03-14 08:42:09 -0700

    Flattened education blocks

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 var fs = require("fs");
 var Handlebars = require("handlebars");
 
+COURSES_COLUMNS = 3;
+
+function validateArray(arr) {
+  return arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0;
+}
+
 function render(resume) {
+  // Split courses into 3 columns
+  if (validateArray(resume.education)) {
+    resume.education.forEach(function(block) {
+      if (validateArray(block.courses)) {
+        splitCourses = [];
+        columnIndex = 0;
+        for (var i = 0; i < COURSES_COLUMNS; i++) {
+          splitCourses.push([]);
+        }
+        block.courses.forEach(function(course) {
+          splitCourses[columnIndex].push(course);
+          columnIndex++;
+          if (columnIndex >= COURSES_COLUMNS) {
+            columnIndex = 0;
+          }
+        });
+        block.courses = splitCourses;
+      }
+    });
+  }
+
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
 	return Handlebars.compile(tpl)({
